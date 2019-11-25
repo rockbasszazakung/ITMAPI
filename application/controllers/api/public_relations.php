dcce156f9bdf499559d6df36cae2b4a4b1ac934b
gamethai1020 <aussamaweelateh@gmail.com>
2019-11-25 15:50:54 +0700

    commit public_relation insert and update API

--- application/controllers/api/public_relations.php
+++ application/controllers/api/public_relations.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Activities extends API_Controller{
-    function __construct(){
-        parent::__construct();
+class Public_relations extends BD_Controller{
+    function construct(){
+        parent::construct();
         $this->load->model('public_relations_model');
     }
     function get_all_get(){
@@ -14,13 +14,57 @@ class Activities extends API_Controller{
             'response' => $result
         ],REST_Controller::HTTP_OK);
 
+    function create_post(){
+        $topic = $this->post('topic');
+        // topic คือ หัวข้อประชาสัมพันธ์
+        $content = $this->post('content');
+        // content คือ เนื้อหาประชาสัมพันธ์
+        $announcer_id = $this->post('announcer_id');
+        // Announcer_name คือ คนประกาศประชาสัมพันธ์
+
+        $data = [
+            'public_id' => null,
+            'topic' => $topic,
+            'content' => $content,
+            'announcer_id' => $announcer_id
+        ];
+
+        $result = $this->public_relations_model->insert($data);
+        $this->response([
+            'status' => true,
+            'response' => $result
+        ],REST_Controller::HTTP_OK);
+    }
+
+    function update_post(){
+        $topic = $this->post('topic');
+        // topic คือ หัวข้อประชาสัมพันธ์
+        $content = $this->post('content');
+        // content คือ เนื้อหาประชาสัมพันธ์
+        $announcer_id = $this->post('announcer_id');
+        // Announcer_name คือ คนประกาศประชาสัมพันธ์
+
+        $data = [
+            'public_id' => null,
+            'topic' => $topic,
+            'content' => $content,
+            'announcer_id' => $announcer_id
+        ];
+
+        $result = $this->apublic_relations_model->update($data);
+        $this->response([
+            'status' => true,
+            'response' => $result
+        ],REST_Controller::HTTP_OK);
+    }
+
         //ไม่สำเร็จ
-    
+
         // $this->response([
         //     'status' => false,
         //     'message' => []
         // ],REST_Controllers::HTTP_CONFLICT);
     }
-    
-    
+
+
 }
--- application/models/Public_relations_model.php
+++ application/models/Public_relations_model.php
@@ -1,12 +1,23 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Activities_model extends CI_Model{
+class Public_relations_model extends CI_Model{
     private $tbl_name = "public_relations";
 
     function get_all($keyword){
         $this->db->like('public_name',$keyword);
         $result = $this->db->get($this->tbl_name);
         return $result->result();
+    }
+
+    function insert($data){
+        return $this->db->insert($this->tbl_name, $data);
+    }
+
+    function update($data){
+        $this->db->where('public_id',$data['public_id']);
+        $this->db->update($this->tbl_name,$data);
+        $result = $this->db->get($this->tbl_name);
+        return $result;
     }
 }
